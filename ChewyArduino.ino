0ce397eae8e32f790bf73ecb0c8563062cf2d2d1
Calum Dingwall <caburum@users.noreply.github.com>
2022-09-19 19:18:49 -0400

    initial commit
    Co-authored-by: AndrewD453 <ajdailey5@gmail.com>

---
+++ ChewyArduino.ino
@@ -0,0 +1,62 @@
+#include <Servo.h>
+
+#define PIN_COMPRESSOR 5
+#define PIN_WHEEL 6
+#define PIN_PLUNGER 7
+#define PIN_GATE 4
+#define PIN_COMPRESSOR_SENSOR 8
+#define SWITCH_ARM 12
+#define SWITCH_FIRE 11
+
+bool primed = false;
+
+Servo spinner;
+
+#define DELAY 500
+
+void setup() {
+	pinMode(PIN_COMPRESSOR, OUTPUT);
+	spinner.attach(PIN_WHEEL, 1000, 2000);
+	spinner.write(1500);
+	pinMode(PIN_PLUNGER, OUTPUT);
+	digitalWrite(PIN_PLUNGER, HIGH);
+	pinMode(PIN_GATE, OUTPUT);
+	digitalWrite(PIN_GATE, HIGH);
+	pinMode(PIN_COMPRESSOR_SENSOR, INPUT);
+	pinMode(SWITCH_ARM, INPUT_PULLUP);
+	pinMode(SWITCH_FIRE, INPUT_PULLUP);
+	Serial.begin(9600);
+}
+
+void loop() {
+	if (digitalRead(PIN_COMPRESSOR_SENSOR)) {
+		digitalWrite(PIN_COMPRESSOR, HIGH);
+	} else {
+		digitalWrite(PIN_COMPRESSOR, LOW);
+	}
+
+	if (digitalRead(SWITCH_ARM)) {
+		Serial.println("Spinning...");
+		spinner.write(2000);
+	} else {
+		Serial.println("Stopping spinner");
+		spinner.write(1500);
+	}
+
+	// pulled to fire, about to go
+	if (!digitalRead(SWITCH_FIRE)) {
+		primed = true;
+	}
+	// fire when released back to arm
+	if (digitalRead(SWITCH_FIRE) && primed) {
+		Serial.println("Shooting!");
+		digitalWrite(PIN_PLUNGER, LOW);
+		delay(1000);
+		digitalWrite(PIN_GATE, LOW);
+		delay(100);
+		digitalWrite(PIN_PLUNGER, HIGH);
+		delay(500);
+		digitalWrite(PIN_GATE, HIGH);
+		primed = false;
+	}
+}
